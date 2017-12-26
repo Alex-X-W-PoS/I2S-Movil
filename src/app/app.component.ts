c175eb19ba44f999496f4fdd2f8758b49e5f3fff
Roberth Loor <roberthloor1@gmail.com>
2017-12-26 15:06:27 -0500

    Vista rol de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform, MenuController, Nav } from 'ionic-angular';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ListPage } from '../pages/list/list';
+import { RolesPage } from '../pages/roles/roles';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
---
+++ src/pages/roles/roles.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,ActionSheetController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-roles',
+  templateUrl: 'roles.html',
+})
+export class RolesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RolesPage');
+  }
+
+}
+
+
+
+import { Component } from '@angular/core';
+
+import { Platform, ActionSheetController } from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'basic.html'
+})
+export class BasicPage {
+  constructor(
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController
+  ) { }
+
+  openMenu() {
+    
+  }
+}
