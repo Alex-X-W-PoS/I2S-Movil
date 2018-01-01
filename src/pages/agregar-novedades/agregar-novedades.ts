e668ec479e622658741260d5d855f1ac2e08ec98
Alex Ferrin <alex-710@hotmail.com>
2018-01-01 10:57:47 -0500

    Cambios a la interfaz, prueba con una imagen de prueba (valga la redundancia)

--- src/pages/agregar-novedades/agregar-novedades.ts
+++ src/pages/agregar-novedades/agregar-novedades.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular'
 
 @Component({
   selector: 'page-agregar-novedades',
   templateUrl: 'agregar-novedades.html'
 })
 export class AgregarNovedadesPage {
-  constructor() {
+	image = '';
+  constructor(public actionSheetCtrl: ActionSheetController) {
 
   }
+
+  presentActionSheet() {
+   let actionSheet = this.actionSheetCtrl.create({
+     buttons: [
+       {
+         text: 'Cámara',
+         icon: 'camera',
+         handler: () => {
+           console.log('Destructive clicked');
+         }
+       },
+       {
+         text: 'Galería',
+         icon: 'image',
+         handler: () => {
+           this.image = "../../assets/imgs/brokenac.jpg";
+         }
+       },
+       {
+         text: 'Cancelar',
+         role: 'cancel',
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       }
+     ]
+   });
+
+   actionSheet.present();
+ }
+
+ borrarImagen(){
+ 	this.image = '';
+ }
 }
