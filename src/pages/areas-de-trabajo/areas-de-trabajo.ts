af64084ed778256d1709f63e4a35cabafab0cbf3
Xavier Idrovo <xidrovo@hotmail.com>
2018-01-16 21:02:11 -0500

    version 0.2.0 del código, Areas de trabajo y puestos de trabajo enlazados

--- src/pages/areas-de-trabajo/areas-de-trabajo.ts
+++ src/pages/areas-de-trabajo/areas-de-trabajo.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { IonicPage, NavController, NavParams } from 'ionic-angular'
 import { HttpProvider } from '../../providers/http/http'
+import { PuestoDeTrabajoPage } from '../puesto-de-trabajo/puesto-de-trabajo'
 
 @IonicPage()
 @Component({
@@ -8,10 +9,21 @@ import { HttpProvider } from '../../providers/http/http'
   templateUrl: 'areas-de-trabajo.html'
 })
 export class AreasDeTrabajoPage {
+  listaDeAreas: any
+  areaValue: any
+  puestoValue: any
   constructor (public navCtrl: NavController, public navParams: NavParams, public puestos: HttpProvider) {
-
   }
-  /*ionViewDidLoad () {
-
-  }*/
+  obtenerAreas () {
+    this.listaDeAreas = ['']
+    this.puestos.obtenerPuestoDeTrabajoDeArea(this.areaValue).then(res => {
+      this.listaDeAreas = res.datos          
+    },
+      error => {
+        console.log(error)
+    }) 
+  }
+  entrarPuestosDeTrabajo () {
+    void this.navCtrl.push(PuestoDeTrabajoPage, this.puestoValue)
+  }
 }
--- src/pages/puesto-de-trabajo/puesto-de-trabajo.ts
+++ src/pages/puesto-de-trabajo/puesto-de-trabajo.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
-import { NavController } from 'ionic-angular'
+import { NavController, NavParams } from 'ionic-angular'
 import { HttpProvider } from '../../providers/http/http'
+import { NovedadesSinAtenderPage } from '../novedades-sin-atender/novedades-sin-atender'
 
 @Component({
   selector: 'page-puesto-de-trabajo',
@@ -13,32 +14,27 @@ export class PuestoDeTrabajoPage {
   public arrayRiesgos: any
   public porcentaje: any
   public idRiesgos: any
-  constructor (public navCtrl: NavController, public classPuesto: HttpProvider) {
-  	this.cargarRiesgos()
+  constructor (public navCtrl: NavController, public navParams: NavParams, public classPuesto: HttpProvider) {
+  	
   }
 
-  /*ionViewDidLoad () {
-  }*/
-
+  ionViewDidLoad () {
+    this.cargarRiesgos()
+  }
   cargarRiesgos () {
-    this.classPuesto.obtenerPuestoDeTrabajo('1').then(res => {
+    this.classPuesto.obtenerPuestoDeTrabajo(this.navParams.data).then(res => {
       this.dataPuestos = res.datos
       this.numEmpleados = this.dataPuestos.num_empleados
       this.numNovedades = this.dataPuestos.num_novedades
       this.arrayRiesgos = this.dataPuestos.valoracion_puesto_trabajo
       this.porcentaje = 0
-      console.log(this.dataPuestos)
+      console.log(this.numEmpleados)
     },
     error => {
       console.log(error)
     })
   }
+  cargarNovedades () {
+    void this.navCtrl.push(NovedadesSinAtenderPage, this.navParams.data)
+  }
 }
-/*
-  this.variablePublic.obtenerArea(id:String).then(res =>{
-          this.VariableQueAlmacene = res.datos;
-        },
-        error =>{
-          console.log(error);
-        });
-*/
