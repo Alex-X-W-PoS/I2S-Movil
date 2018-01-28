9bcd9bd3354274370d36e24f485d6dfa52acaf29
Joel Rodriguez <joelerll@gmail.com>
2018-01-28 02:53:18 -0500

    update buscar text, Limpiar label tipo prioridad cuando no esta ninguna novedad

--- src/pages/novedades/novedades.ts
+++ src/pages/novedades/novedades.ts
@@ -12,6 +12,7 @@ import moment from 'moment'
 export class NovedadesPage {
   segment = 'sinAtender'
   queryText = ''
+  queryTextNoAtendidas = ''
   items: any
   itemAtendidos: any
   constructor (public navCtrl: NavController, public navParams: NavParams, public global: GlobalProvider) {
@@ -57,10 +58,24 @@ export class NovedadesPage {
   detallesNovedades (item) {
     void this.navCtrl.push(AtenderNovedadPage, { item: item , 'puesto': this.navParams.get('puestoId') })
   }
+  public novedadesPrioridad (novedades, prioridad) {
+    for (let i = novedades.length - 1; i >= 0; --i) {
+      if (novedades[i].prioridad === prioridad) {
+        return true
+      }
+    }
+    return false
+  }
   swipeEvent (e) {
-    if (e.direction === 2) {
+    // devido a problemas con el swipe al hacer hacia abajo
+    // se debe usar la propiedad angle para determinar
+    // cuando se hace para la derecha o izquierda.
+    // Se la encontro usando console.log(`${JSON.stringify(e)}`)
+    let ANGULO_DERECHA_MINIMO = 0
+    let ANGULO_IZQUIERDA_MAXIMO = 10
+    if (e.direction === 2 && e.angle > ANGULO_DERECHA_MINIMO) {
       this.segment = 'atendidas'
-    } else if (e.direction === 4) {
+    } else if (e.direction === 4 && e.angle < ANGULO_IZQUIERDA_MAXIMO) {
       this.segment = 'sinAtender'
     }
   }
